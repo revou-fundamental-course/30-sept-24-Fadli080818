3ee37d5670b083b7d673d7cf47049b8874f09235
Muhammad Fadli <muhammadfadli080818@gmail.com>
2024-10-10 00:37:19 +0800

    merubah style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// Public Variable for Alert Box
+let alertBox = document.getElementById("customAlertBox");
+let alert_Message_container = document.getElementById("alertMessage");
+let close_img = document.querySelector("close");
+
+// Function Reset Button Click
 function btnReverse() {
   var idConvert = document.getElementById("convertRole").value;
   if (idConvert == 0) {
@@ -5,7 +11,7 @@ function btnReverse() {
     document.getElementById("text1").innerHTML = "Fahrenheit (&degF)";
     document.getElementById("text2").innerHTML = "Celcius (&degC)";
     document.getElementById("text-convert").innerHTML =
-      "Fahrenheit (&degF) ke Celcius (&degC)";
+      "Fahrenheit (&degF) to Celcius (&degC)";
 
     let no1 = document.getElementById("text-input").value;
     let no2 = document.getElementById("text-result").value;
@@ -27,7 +33,7 @@ function btnReverse() {
     document.getElementById("text1").innerHTML = "Celcius (&degC)";
     document.getElementById("text2").innerHTML = "Fahrenheit (&degF)";
     document.getElementById("text-convert").innerHTML =
-      "Celcius (&degC) ke Fahrenheit (&degF)";
+      "Celcius (&degC) to Fahrenheit (&degF)";
 
     let no1 = document.getElementById("text-input").value;
     let no2 = document.getElementById("text-result").value;
@@ -47,6 +53,7 @@ function btnReverse() {
   }
 }
 
+// Function Reset Button Click
 function btnReset() {
   document.getElementById("text-input").value = "";
   document.getElementById("text-result").value = "";
@@ -56,39 +63,65 @@ function btnReset() {
     "Celcius (&degC) ke Fahrenheit (&degF)";
   document.getElementById("text-calculate").value = "S = Value * (9 / 5) + 32";
   document.getElementById("text-input").focus();
+  document.getElementById("convertRole").value = 0;
 }
 
+// Function Covert Button Click
 function btnConvert() {
   let textInput = document.getElementById("text-input").value;
 
   if (textInput == "") {
-    alert("Please enter a convertion number.");
+    alert_Message_container.innerHTML = "Please input a conversion number !";
+    alertBox.style.display = "block";
+    document.getElementById("text-input").focus();
   } else if (
     parseInt(textInput) != textInput &&
     parseFloat(textInput) != textInput
   ) {
-    alert("Please enter a valid number.");
+    alert_Message_container.innerHTML = "Please input a valid number !";
+    alertBox.style.display = "block";
     document.getElementById("text-input").value = "";
     document.getElementById("text-input").focus();
   } else {
     var idConvert = document.getElementById("convertRole").value;
 
     if (idConvert == "0") {
-      var convertValue = document.getElementById("text-input").value;
+      var convertValue = parseFloat(
+        document.getElementById("text-input").value
+      ).toFixed(0);
+      document.getElementById("text-input").value = convertValue;
       document.getElementById("text-result").value = parseFloat(
         convertValue * (9 / 5) + 32
-      ).toFixed(2);
+      ).toFixed(0);
 
       document.getElementById("text-calculate").value =
         "S = " + convertValue + " * (9 / 5) + 32";
     } else if (idConvert == "1") {
-      var convertValue = document.getElementById("text-input").value;
+      var convertValue = parseFloat(
+        document.getElementById("text-input").value
+      ).toFixed(0);
+      document.getElementById("text-input").value = convertValue;
       document.getElementById("text-result").value = parseFloat(
         (convertValue - 32) * (5 / 9)
-      ).toFixed(2);
+      ).toFixed(0);
 
       document.getElementById("text-calculate").value =
         "S = " + "(" + convertValue + " - 32)" + " * (5 / 9)";
     }
   }
 }
+
+// Click to Close Alert
+function closeAlert() {
+  alertBox.style.display = "none";
+}
+
+// Function Close Alert by click anywhere not on the alert box
+document.addEventListener("click", function (e) {
+  if (e.target == alertBox) {
+    alertBox.style.display = "none";
+    document.getElementById("text-input").focus();
+  }
+
+  document.getElementById("text-input").focus();
+});
